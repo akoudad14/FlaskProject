6ea13d15c7b19a5984a2532c92ec94d630ea55c5
Marvin Akoudad <m.akoudad.perso@gmail.com>
2022-03-13 21:30:18 +0100

    Revert "new DB design comment"
    This reverts commit e51556c8

--- database/models/CharacterEpisodeComment.py
+++
@@ -1,9 +0,0 @@
-
-from database import db
-
-character_episode_comment = \
-    db.Table('character_episode_comment', db.metadata,
-             db.Column('character_episode_id', db.Integer,
-                       db.ForeignKey('character_episode.id')),
-             db.Column('comment_id', db.Integer,
-                       db.ForeignKey('comment.id')))
